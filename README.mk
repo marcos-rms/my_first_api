d7e21e260f2733e9030d4f3869581f54b92c1e14
marcos-rms <marcos_ramos91@hotmail.com>
2024-12-06 21:52:05 -0300

    test

---
+++ README.mk
@@ -0,0 +1,9 @@
+1. Instalar o git
+	git init
+	criar repositório no GitHub
+	git remote add origin <endereço do repositório no GitHub>
+	git pull origin main
+	git checkou -b main
+	git config --global user.email "marcos_ramos91@hotmail.com"
+  	git config --global user.name "marcos_rms"
+2. Instalar e configurar o arquivo package.json
